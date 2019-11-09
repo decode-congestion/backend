fb66e00922ea9e29ea52342f783d0cdfbe70dab1
Andy <github@sockbot.com>
2019-11-09 14:02:51 -0800

    connect backend to transit-service db

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db = require("./data/db.js");
 
 app.get("/pins/:lat/:lon", (req, res) => {
   const lat = req.params.lat;
   const lon = req.params.lon;
-  return res.send("Lat: " + lat + "Lon: " + lon);
+  return res.send("Lat: " + lat + " Lon: " + lon);
+});
+
+app.get("/api/routes", async (req, res) => {
+  const routes = await db("routes");
+  res.json({ routes });
 });
 
 app.get("/", (req, res) => res.send("Hello World!"));
---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "bus_jousting",
+      user: "ajchan",
+      password: "bus_jousting"
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
