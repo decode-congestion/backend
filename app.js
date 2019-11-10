dd419479475471d2b1ef53f0bc60040dce2bf4b4
Andy <github@sockbot.com>
2019-11-10 01:02:53 -0800

    add get endpoint for managing roster

--- app.js
+++ app.js
@@ -7,24 +7,46 @@ const { listenToSockets } = require("./main/listener.js");
 const { knex, st } = require("./data/knexSetup");
 
 app.get("/pins/:lat/:lon", (req, res) => {
-    const lat = req.params.lat;
-    const lon = req.params.lon;
-    return res.send("Lat: " + lat + " Lon: " + lon);
+  const lat = req.params.lat;
+  const lon = req.params.lon;
+  return res.send("Lat: " + lat + " Lon: " + lon);
 });
-app.get('/api/users/:userId/collected', async (req, res) => {
-    const userId = req.params['userId'];
+app.get("/api/users/:userId/collected", async (req, res) => {
+  const userId = req.params["userId"];
 
-    res.json({
-        cv: await knex.select('vehicle_id').from('collected_vehicles').where('user_id', '=', userId),
-        loots: await knex.select('*').from('loots').where({
-            user_id: userId,
-            vehicle_id: null
-        })
-    });
+  res.json({
+    cv: await knex
+      .select("vehicle_id")
+      .from("collected_vehicles")
+      .where("user_id", "=", userId),
+    loots: await knex
+      .select("*")
+      .from("loots")
+      .where({
+        user_id: userId,
+        vehicle_id: null
+      })
+  });
 });
+app.get("/api/users/:userId/roster_vehicles", async (req, res) => {
+  const userId = req.params.userId;
+
+  res.json({
+    roster_vehicles: await knex
+      .select("vehicle_id")
+      .from("roster_vehicles")
+      .where("user_id", "=", userId),
+    loots: await knex
+      .select("*")
+      .from("loots")
+      .where("user_id", userId)
+      .whereNot("vehicle_id", null)
+  });
+});
+// app.post("/api/users/:userId/roster_vehicles");
 app.get("/api/routes", async (req, res) => {
-    const routes = await db("routes");
-    res.json({routes});
+  const routes = await db("routes");
+  res.json({ routes });
 });
 app.get("/", (req, res) => res.send("Hello World!"));
 
